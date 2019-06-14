cd9bc10371a62dcc7832938c95cc4b77f8c95fa0
大大大黄呀 <835814273@qq.com>
2019-06-14 12:53:58 +0800

    add files

--- 算法/leetcode/6.14_242有效字母异位词/index.js
+++ 算法/leetcode/6.14_242有效字母异位词/index.js
@@ -1,13 +1,33 @@
 var isAnagram = function(s, t) {
 
-  var apartS = s.split('').sort().join()
-
-  var apartT = t.split('').sort().join()
-
-    var a = new RegExp(apartS)
-
-  console.log(a.test(apartT)) 
-
-
+  // var apartS = s.split('').sort().join()
+  // var apartT = t.split('').sort().join()
+  //   console.log(apartS)
+  //   var a = new RegExp(apartS)
+  //   console.log(a)
+  //   // var b = a.test(apartT)
+  //   return a.test(apartT)
+  
+  // return isAnagram('rat','art')
+  // if(s.length !== t.length) 
+  // {
+  //   return false;
+  // }
+  // else
+  // {
+  //   var apartS = s.split('').sort().join()
+  // var apartT = t.split('').sort().join()
+  // return apartS === apartT
+  // }
+  if(s.length == t.length)
+  {
+    var apartS = s.split('').sort().join()
+    var apartT = t.split('').sort().join()
+   return apartS === apartT
+  }
+  else
+  {
+    return false
+  }
 };
-isAnagram('rat','art')
+console.log(isAnagram('rat','art'))
