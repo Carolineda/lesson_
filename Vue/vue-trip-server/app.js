f1be35eaab3fd179c91496596f10e2e504ea0894
大大大黄呀 <835814273@qq.com>
2019-07-29 20:21:07 +0800

    add files

--- Vue/vue-trip-server/app.js
+++ Vue/vue-trip-server/app.js
@@ -1,13 +1,49 @@
 // 引入资源文件
-  var createError = require('http-errors')         //express中间件
-  var express = require('express')
-  var path = require('path')
-  var cookieParser = require('cookie-parser')
-  var looger = require('morgan')
+var createError = require('http-errors');         //express中间件
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var looger = require('morgan');
 
 
 // 引入index.js  路由配置文件 
-  var indexRouter = require('./routes/index')
+var indexRouter = require('./routes/index');
 
 // 引入user.js路由配置文件
-var userRouter = require('./routes/user.js')
+var userRouter = require('./routes/user');
+
+// 创建引用
+var app = express();     //espress框架指的是app
+
+app.set('views', path.join(__dirname, 'views'));    //指定视图文件夹     node引入文件
+app.set('view engine', 'pug');       //pug  HTML框架 指定视图引擎
+
+// 使用刚刚加载的资源
+app.use(looger('dev'));   //帮助生成日志
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));   //urlencoded是express中间件 默认不扩展在此
+app.use(cookieParser());  //使用cookie
+app.use(express.static(path.join(__dirname, 'public')));   //指定公共资源文件为public
+
+app.use('/', indexRouter);    //获取当前页面路径  当路径为根路径 也就是'http://localhost:3000'匹配index.js 使用index。js里面的逻辑
+app.use('/user', userRouter);      //当路径为/user也就是'http://localhost:3000/user'匹配user.js 使用user.js里面的逻辑
+
+// 匹配404 和路由未匹配时
+app.use(function (req, res, next) {
+  next(createError(404));
+})
+
+// 当路径匹配错误时 
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};        //开发环境下看到错误
+
+  // 当页面渲染错误时 
+  res.status(err.status || 500);
+  res.render('error');
+})
+
+
+console.log('运行成功');
+
+module.exports = app;
--- Vue/vue-trip-server/routes/user.js
+++ Vue/vue-trip-server/routes/user.js
@@ -3,16 +3,58 @@ var router = express.Router()
 var mysql = require('mysql')
 
 var data = {
-  code:200,
-  msg:'success'
+  code: 200,
+  msg: 'success'
 }
 
-var pool = mysql.createPool({    //创建mysql连接
-  host:'localhost',
-  user:'root',
-  password:'root',
-  database:'trip'
+var pool = mysql.createPool({    //创建mysql连接  连接库
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'trip'
 })
 
 
 // 开始请求
+router.post('/', function (req, res, next) {
+  let userName = req.body.userName           //前端发来请求携带参数  前端点击登录
+  let password = req.body.passWord
+  // 从数据库查找数据username\password，最后返回给前端
+  pool.query(`SELECT * FROM user where userName = ${userName}`, function (err, results, fields) {
+    if (err) {
+      data.code = 500    //服务器出现问题
+      data.msg = err
+    }
+    if (results[0].passWord === password) {
+      selectUser(results[0].userId)
+    } else {
+      data.code = 400
+      data.msg = '账号或密码有误'
+      res.statusCode = 200
+      res.setHeader('Access-Control-Allow-Origin', '*')             //设置请求头
+      res.setHeader('Content-Type', 'application/json')             //设置请求头的格式
+      res.json(data)               //让data返回数据的格式是json格式
+    }
+  })
+
+  function selectUser(id) {
+    pool.query(`SELECT * FROM user_info where userId = ${id}`, function (err, results, fields) {
+      //query查询数据库
+      if (err) {
+        data.code = 500
+        data.msg = err
+      }
+      data.code = 200
+      data.msg = 'success'
+      data.data = results[0]                  //置于位置
+      res.statusCode = 200
+      res.setHeader('Access-Control-Allow-Origin', '*')             //设置请求头
+      res.setHeader('Content-Type', 'application/json')             //设置请求头的格式
+      res.json(data)
+    })
+
+
+  }
+})
+
+module.exports = router
