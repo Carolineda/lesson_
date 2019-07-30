8bfed70faa8ca516bbbfd83506db3766b44d3b60
大大大黄呀 <835814273@qq.com>
2019-07-30 22:15:40 +0800

    add files

---
+++ letcode_算法/leaf_二叉树/index.js
@@ -0,0 +1,32 @@
+
+var sumNumbers = function (root) {
+  return helper(root, 0)
+}
+
+// 递归
+function helper(node, cur) {
+  // 递归的退出条件
+  if (node === null) return 0;
+  const next = node.val + cur * 10
+  if (node.left === null && node.right === null) {
+    return next    //叶子结点  退出条件
+  }
+  const l = helper(node.left, next)
+  const r = helper(node.right, next)
+  return l + r
+}
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null
+}
+
+const a1 = new TreeNode(1)
+const a2 = new TreeNode(2)
+const a3 = new TreeNode(3)
+a1.left = a2
+a1.right = a3
+
+
+// 递归完成
+console.log(sumNumbers(a1))
